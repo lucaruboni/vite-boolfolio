14843ee948f5de683e06da16ac66a5249ff51c33
lucaruboni <lucaruboni@gmail.com>
2023-06-13 18:45:41 +0200

    [add] quite complete ex

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,76 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+
 
 export default {
     components: {
-        HelloWorld,
+     
+    },
+    data(){
+        return{
+            base_API: 'http//127.0.0.1:8000/',
+            projects_API: 'api/projects',
+            loading: true,
+            projects: null,
+            error: null
+        }
+    },
+    methods:{
+        getProjects(url){
+            axios
+            .get(url)
+            .then(response=> {
+                this.projects = response.data.projects
+                this.loading = false
+            })
+            .catch(error => {
+                console.log(error);
+                this.error = error.message
+            })
+            },
+
+            getImageFromPath(path){
+                return this.base_url + /* 'storage/' + */ path;
+            }
+        
+    },
+    mounted(){
+        const url = this.base_API +   this.projects_API
+        this.getProjects(url)
     }
 }
 </script>
 
 <template>
-    <section class="vue-home">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 d-flex flex-column justify-content-center align-items-center vh-100">
-                    <div class="logos">
-                        <a href="https://vitejs.dev" target="_blank">
-                            <img src="/vite.svg" class="logo" alt="Vite logo" />
-                        </a>
-                        <a href="https://vuejs.org/" target="_blank">
-                            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-                        </a>
-                    </div>
-                    <HelloWorld />
+<div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container-fluid py-5">
+      <h1 class="display-5 fw-bold">My Blog</h1>
+      <p class="col-md-8 fs-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus mollitia deleniti optio eaque iste porro, placeat numquam inventore blanditiis saepe!</p>
+        
+    </div>
+  </div>
+
+  <section class="projects">
+    <div class="container">
+        <div class="row">
+            <div class="col" v-for="project in projects">
+                <div class="card text-start">
+                  <img class="card-img-top" :src="getImageFromPath()" alt="Title">
+                  <div class="card-body h-100">
+                    <h4 class="card-title">{{ project.title }}</h4>
+                    <p class="card-text"></p>
+                  </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+
+  </section>
 </template>
 
 <style lang="scss">
 @use './styles/general.scss';
 
-.vue-home {
-    color: #2c3e50;
-    background: #181818;
-    transition: color 0.5s, background-color 0.5s;
-    line-height: 1.6;
-    font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
-        Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-    font-size: 15px;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
 
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-}
-
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
